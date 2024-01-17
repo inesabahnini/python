d72a4a1d2928fd7a08c90014db5af448bd67e8ee
unknown <abahniniines@gmail.com>
2024-01-17 19:49:57 +0100

    variable, type, print, input,conversion

---
+++ seance1/exemple1.py
@@ -0,0 +1,10 @@
+# type str 
+nom="Ali"
+#type float
+salaire=1200.7654
+#type int
+age=35
+#boolean
+celibataire=True
+#affiche
+print("l'employee",nom,"a comme salaire" ,salaire)
---
+++ seance1/exemple2.py
@@ -0,0 +1,5 @@
+# lire a partir du clavier 
+nom =input("entrer le nom")
+salaire=input("entrer le salaire")
+age=input("entrer l'age ")
+print("l'employee",nom,"a comme salaire" ,salaire,age)
